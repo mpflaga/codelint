f2f1b3d838c11681326c02a7b89203d2e01f7dab
Michael P. Flaga <mflaga@tnsi.com>
2024-11-07 00:54:43 +0000

    fixed pylama

--- example-black.py
+++ example-black.py
@@ -1,15 +1,10 @@
-# Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(
-    var_one, var_two, var_three, var_four, var_five, var_six, var_seven
-)
-
-
 # Further indentation required as indentation is not distinguishable.
 def long_function_name(var_one, var_two, var_three, var_four):
     print(var_one)
 
 
-result = some_function_that_takes_arguments(
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_function_name(
     "a",
     "b",
     "c",
@@ -17,6 +12,9 @@ result = some_function_that_takes_arguments(
     "e",
     "f",
 )
+
+
+result = long_function_name("a", "b", "c", "d", "e", "f",)
 
 my_list = [
     1,
